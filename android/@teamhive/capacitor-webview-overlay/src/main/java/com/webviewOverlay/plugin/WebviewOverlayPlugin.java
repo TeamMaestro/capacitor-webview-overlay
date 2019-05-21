f5fe397fafe84e486dcf44ce9512418d511bd34c
Sean Perkins <sean@devonite.com>
2019-05-21 18:08:05 -0400

    fix(deps): v1.6.1

--- android/@teamhive/capacitor-webview-overlay/src/main/java/com/webviewOverlay/plugin/WebviewOverlayPlugin.java
+++ android/@teamhive/capacitor-webview-overlay/src/main/java/com/webviewOverlay/plugin/WebviewOverlayPlugin.java
@@ -6,6 +6,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.MimeTypeMap;

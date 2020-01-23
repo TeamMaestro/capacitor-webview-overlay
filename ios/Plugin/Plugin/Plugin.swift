67fc980c31baa4ac8ec182eb6c9f104e79d60d99
Sean Perkins <sean@meetmaestro.com>
2020-01-23 11:42:23 -0500

    fix(ios): null check 1.6.3

--- ios/Plugin/Plugin/Plugin.swift
+++ ios/Plugin/Plugin/Plugin.swift
@@ -4,46 +4,46 @@ import GCDWebServer
 
 @available(iOS 11.0, *)
 class WebviewOverlay: UIViewController, WKUIDelegate, WKNavigationDelegate {
-    
+
     var webview: WKWebView?
     var plugin: WebviewOverlayPlugin!
     var configuration: WKWebViewConfiguration!
-    
+
     var closeFullscreenButton: UIButton!
     var topSafeArea: CGFloat!
-    
+
     var webServer: GCDWebServer?
-    
+
     var currentDecisionHandler: ((WKNavigationResponsePolicy) -> Void)? = nil
-    
+
     var openNewWindow: Bool = false
-    
+
     var currentUrl: URL?
-    
+
     var loadUrlCall: CAPPluginCall?
-    
+
     init(_ plugin: WebviewOverlayPlugin, configuration: WKWebViewConfiguration) {
         super.init(nibName: "WebviewOverlay", bundle: nil)
         self.plugin = plugin
         self.configuration = configuration
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func loadView() {
         self.webview = WKWebView(frame: .zero, configuration: self.configuration)
         self.webview?.uiDelegate = self
         self.webview?.navigationDelegate = self
-        
+
         view = self.webview
         view.isHidden = plugin.hidden
         self.webview?.scrollView.bounces = false
         self.webview?.allowsBackForwardNavigationGestures = true
-        
+
         self.webview?.isOpaque = false
-        
+
         let button = UIButton(frame: CGRect(x: UIScreen.main.bounds.width - 60, y: 20, width: 40, height: 40))
         let image = UIImage(named: "icon", in: Bundle(for: NSClassFromString("WebviewOverlayPlugin")!), compatibleWith: nil)
         button.setImage(image, for: .normal)
@@ -52,7 +52,7 @@ class WebviewOverlay: UIViewController, WKUIDelegate, WKNavigationDelegate {
         button.adjustsImageWhenHighlighted = false
         button.layer.cornerRadius = 0.5 * button.bounds.size.width
         button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
-        
+
         let blur = UIVisualEffectView(effect: UIBlurEffect(style: UIBlurEffect.Style.regular))
         blur.frame = button.bounds
         blur.layer.cornerRadius = 0.5 * button.bounds.size.width
@@ -60,22 +60,22 @@ class WebviewOverlay: UIViewController, WKUIDelegate, WKNavigationDelegate {
         blur.isUserInteractionEnabled = false
         button.insertSubview(blur, at: 0)
         button.bringSubviewToFront(button.imageView!)
-        
+
         self.closeFullscreenButton = button
         view.addSubview(self.closeFullscreenButton)
-        
+
         self.webview?.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
     }
-    
+
     override func viewDidLayoutSubviews() {
         self.topSafeArea = view.safeAreaInsets.top
         self.closeFullscreenButton.frame = CGRect(x: UIScreen.main.bounds.width - 60, y: self.topSafeArea + 20, width: 40, height: 40)
     }
-    
+
     @objc func buttonAction(sender: UIButton!) {
         plugin.toggleFullscreen()
     }
-    
+
     public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         currentUrl = webView.url
         view.isHidden = plugin.hidden
@@ -87,7 +87,7 @@ class WebviewOverlay: UIViewController, WKUIDelegate, WKNavigationDelegate {
             self.loadUrlCall = nil
         }
         plugin.notifyListeners("pageLoaded", data: [:])
-        
+
         // Remove tap highlight
         let script = "function addStyleString(str) {" +
             "var node = document.createElement('style');" +
@@ -97,7 +97,7 @@ class WebviewOverlay: UIViewController, WKUIDelegate, WKNavigationDelegate {
         "addStyleString('html, body {-webkit-tap-highlight-color: transparent;}');"
         webView.evaluateJavaScript(script)
     }
-    
+
     public func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
         if navigationAction.targetFrame == nil, let url = navigationAction.request.url {
             if (plugin.hasListeners("navigationHandler")) {
@@ -107,18 +107,18 @@ class WebviewOverlay: UIViewController, WKUIDelegate, WKNavigationDelegate {
         }
         return nil
     }
-    
+
     public func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
         self.clearDecisionHandler()
     }
-    
+
     func clearDecisionHandler() {
         if (self.currentDecisionHandler != nil) {
             self.currentDecisionHandler!(.allow)
             self.currentDecisionHandler = nil
         }
     }
-    
+
     public func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
         if (self.currentDecisionHandler != nil) {
             self.clearDecisionHandler()
@@ -137,7 +137,7 @@ class WebviewOverlay: UIViewController, WKUIDelegate, WKNavigationDelegate {
             return
         }
     }
-    
+
     public func clearWebServer() {
         if (self.webServer != nil) {
             if (self.webServer?.isRunning == true) {
@@ -146,7 +146,7 @@ class WebviewOverlay: UIViewController, WKUIDelegate, WKNavigationDelegate {
             self.webServer = nil
         }
     }
-    
+
     public func loadUrl(_ url: URL) {
         if url.absoluteString.hasPrefix("file") {
             self.clearWebServer()
@@ -166,7 +166,7 @@ class WebviewOverlay: UIViewController, WKUIDelegate, WKNavigationDelegate {
             self.webview?.load(URLRequest(url: url))
         }
     }
-    
+
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         if (keyPath == "estimatedProgress") {
             plugin.notifyListeners("progress", data: ["value":self.webview?.estimatedProgress ?? 1])
@@ -178,30 +178,30 @@ class WebviewOverlay: UIViewController, WKUIDelegate, WKNavigationDelegate {
 @available(iOS 11.0, *)
 @objc(WebviewOverlayPlugin)
 public class WebviewOverlayPlugin: CAPPlugin {
-    
+
     var width: CGFloat!
     var height: CGFloat!
     var x: CGFloat!
     var y: CGFloat!
-    
+
     var hidden: Bool = false
-    
+
     var fullscreen: Bool = false
-    
+
     var webviewOverlay: WebviewOverlay!
-    
+
     /**
      * Capacitor Plugin load
      */
     override public func load() {}
-    
+
     @objc func open(_ call: CAPPluginCall) {
         DispatchQueue.main.async {
             let webConfiguration = WKWebViewConfiguration()
             webConfiguration.allowsInlineMediaPlayback = true
             webConfiguration.mediaTypesRequiringUserActionForPlayback = []
             webConfiguration.preferences.setValue(true, forKey: "allowFileAccessFromFileURLs")
-            
+
             // Content controller
             let javascript = call.getString("javascript") ?? ""
             if (javascript != "") {
@@ -222,29 +222,29 @@ public class WebviewOverlayPlugin: CAPPlugin {
                 contentController.addUserScript(script)
                 webConfiguration.userContentController = contentController
             }
-            
+
             self.webviewOverlay = WebviewOverlay(self, configuration: webConfiguration)
-            
+
             guard let urlString = call.getString("url") else {
                 call.error("Must provide a URL to open")
                 return
             }
 
             let url = URL(string: urlString)
-            
+
             self.hidden = false
-            
+
             self.width = CGFloat(call.getFloat("width") ?? 0)
             self.height = CGFloat(call.getFloat("height") ?? 0)
             self.x = CGFloat(call.getFloat("x") ?? 0)
             self.y = CGFloat(call.getFloat("y") ?? 0)
-        
+
             self.webviewOverlay.view.isHidden = true
             self.bridge.viewController.addChild(self.webviewOverlay)
             self.bridge.viewController.view.addSubview(self.webviewOverlay.view)
             self.webviewOverlay.view.frame = CGRect(x: self.x, y: self.y, width: self.width, height: self.height)
             self.webviewOverlay.didMove(toParent: self.bridge.viewController)
-        
+
             self.webviewOverlay.loadUrl(url!)
         }
     }
@@ -260,14 +260,14 @@ public class WebviewOverlayPlugin: CAPPlugin {
             }
         }
     }
-    
+
     @objc func getSnapshot(_ call: CAPPluginCall) {
         DispatchQueue.main.async {
             if (self.webviewOverlay != nil) {
                 if (self.webviewOverlay.webview != nil) {
                     let offset: CGPoint = (self.webviewOverlay.webview?.scrollView.contentOffset)!
                     self.webviewOverlay.webview?.scrollView.setContentOffset(offset, animated: false)
-                    
+
                     self.webviewOverlay.webview?.takeSnapshot(with: nil) {image, error in
                         if let image = image {
                             guard let jpeg = image.jpegData(compressionQuality: 1) else {
@@ -289,14 +289,14 @@ public class WebviewOverlayPlugin: CAPPlugin {
             }
         }
     }
-    
+
     @objc func updateDimensions(_ call: CAPPluginCall) {
         DispatchQueue.main.async {
             self.width = CGFloat(call.getFloat("width") ?? 0)
             self.height = CGFloat(call.getFloat("height") ?? 0)
             self.x = CGFloat(call.getFloat("x") ?? 0)
             self.y = CGFloat(call.getFloat("y") ?? 0)
-            
+
             if (!self.fullscreen) {
                 let rect = CGRect(x: self.x, y: self.y, width: self.width, height: self.height)
                 self.webviewOverlay.view.frame = rect
@@ -307,16 +307,16 @@ public class WebviewOverlayPlugin: CAPPlugin {
                 let rect = CGRect(x: 0, y: 0, width: width, height: height)
                 self.webviewOverlay.view.frame = rect
             }
-            
+
             self.webviewOverlay.closeFullscreenButton.frame = CGRect(x: UIScreen.main.bounds.width - 60, y: self.webviewOverlay.topSafeArea + 20, width: 40, height: 40)
-            
+
             if (self.hidden) {
                 self.notifyListeners("updateSnapshot", data: [:])
             }
             call.success()
         }
     }
-    
+
     @objc func show(_ call: CAPPluginCall) {
         DispatchQueue.main.async {
             self.hidden = false
@@ -326,7 +326,7 @@ public class WebviewOverlayPlugin: CAPPlugin {
             call.success()
         }
     }
-    
+
     @objc func hide(_ call: CAPPluginCall) {
         DispatchQueue.main.async {
             self.hidden = true
@@ -343,28 +343,33 @@ public class WebviewOverlayPlugin: CAPPlugin {
                 call.error("Must provide javascript string")
                 return
             }
-            if (self.webviewOverlay.webview != nil) {
-                func eval(completionHandler: @escaping (_ response: String?) -> Void) {
-                    self.webviewOverlay.webview?.evaluateJavaScript(String(javascript)) { (value, error) in
-                        if error != nil {
-                            call.error(error?.localizedDescription ?? "unknown error")
-                        }
-                        else if let valueName = value as? String {
-                            completionHandler(valueName)
+            if (self.webviewOverlay != nil) {
+                if (self.webviewOverlay.webview != nil) {
+                    func eval(completionHandler: @escaping (_ response: String?) -> Void) {
+                        self.webviewOverlay.webview?.evaluateJavaScript(String(javascript)) { (value, error) in
+                            if error != nil {
+                                call.error(error?.localizedDescription ?? "unknown error")
+                            }
+                            else if let valueName = value as? String {
+                                completionHandler(valueName)
+                            }
                         }
                     }
-                }
 
-                eval(completionHandler: { response in
-                    call.resolve(["result": response as Any])
-                })
+                    eval(completionHandler: { response in
+                        call.resolve(["result": response as Any])
+                    })
+                }
+                else {
+                    call.resolve(["result": ""])
+                }
             }
             else {
                 call.resolve(["result": ""])
             }
         }
     }
-    
+
     @objc func toggleFullscreen(_ call: CAPPluginCall? = nil) {
         DispatchQueue.main.async {
             if (self.webviewOverlay != nil) {
@@ -388,7 +393,7 @@ public class WebviewOverlayPlugin: CAPPlugin {
             }
         }
     }
-    
+
     @objc func goBack(_ call: CAPPluginCall) {
         DispatchQueue.main.async {
             if (self.webviewOverlay != nil) {
@@ -397,7 +402,7 @@ public class WebviewOverlayPlugin: CAPPlugin {
             }
         }
     }
-    
+
     @objc func goForward(_ call: CAPPluginCall) {
         DispatchQueue.main.async {
             if (self.webviewOverlay != nil) {
@@ -406,7 +411,7 @@ public class WebviewOverlayPlugin: CAPPlugin {
             }
         }
     }
-    
+
     @objc func reload(_ call: CAPPluginCall) {
         DispatchQueue.main.async {
             if (self.webviewOverlay != nil) {
@@ -415,7 +420,7 @@ public class WebviewOverlayPlugin: CAPPlugin {
             }
         }
     }
-    
+
     @objc func loadUrl(_ call: CAPPluginCall) {
         DispatchQueue.main.async {
             if (self.webviewOverlay != nil) {
@@ -425,7 +430,7 @@ public class WebviewOverlayPlugin: CAPPlugin {
             }
         }
     }
-    
+
     @objc func handleNavigationEvent(_ call: CAPPluginCall) {
         if (self.webviewOverlay != nil && self.webviewOverlay.currentDecisionHandler != nil) {
             if (call.getBool("allow") ?? true) {

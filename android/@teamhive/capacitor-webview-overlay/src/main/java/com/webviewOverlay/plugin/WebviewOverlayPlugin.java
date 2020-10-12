01699375f795950092d2b43bf0fbb1ec936c7e8a
Sean Bannigan <bannigan@meetmaestro.com>
2020-10-12 15:33:16 -0400

    fix(android): update FAB import

--- android/@teamhive/capacitor-webview-overlay/src/main/java/com/webviewOverlay/plugin/WebviewOverlayPlugin.java
+++ android/@teamhive/capacitor-webview-overlay/src/main/java/com/webviewOverlay/plugin/WebviewOverlayPlugin.java
@@ -5,7 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.os.Message;
-import android.support.design.widget.FloatingActionButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
